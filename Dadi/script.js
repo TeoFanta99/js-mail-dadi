84700031ea3ee0855022b6cdaa63290d79dbef17
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-22 16:31:30 +0100

    selezione: primi 3 elementi dom

--- Dadi/script.js
+++ Dadi/script.js
@@ -4,3 +4,16 @@ const humanNumber = document.getElementById("numero-umano");
 const computerNumber = document.getElementById("numero-computer");
 
 
+// generare un numero random per l'utente
+console.log(Math.floor(Math.random() * 6) + 1);
+
+
+// generare un numero random per il computer
+
+
+
+// confronto dei due numeri: chi ha fatto il punteggio più alto?
+    // se il numero dell'utente è > del numero del pc = utente winner
+    // se il numero del pc è > del numero dell'utente = pc winner
+    // se i due numeri sono uguali = pareggio
+    
