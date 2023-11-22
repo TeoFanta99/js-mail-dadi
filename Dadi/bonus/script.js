2c04a3c77f65b9cd1c9fd0da9ee9240a5baf93a0
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-22 17:33:01 +0100

    select: hidden container

--- Dadi/bonus/script.js
+++ Dadi/bonus/script.js
@@ -1,3 +1,11 @@
+// selezionare il bottone per il lancio
+const roll = document.getElementById("bottone-lancia");
+
+
+// selezionare il container nascosto
+const hiddenContainer = document.getElementById("hidden-container")
+
+
 // generare un numero random per l'utente
 var humanNumber = (Math.floor(Math.random() * 6) + 1);
 console.log(humanNumber);
@@ -11,18 +19,29 @@ document.getElementById("numero-computer").innerHTML = computerNumber;
 
 var result = document.getElementById("esito");
 
+roll.addEventListener("click",
 
-// stabilire le condizioni di vittoria, sconfitta e pareggio
-if (humanNumber > computerNumber) {
-    console.log("hai vinto");
-    document.getElementById("esito").innerHTML = "HAI VINTO";
-    result.classList.add("win");
-} else if (computerNumber > humanNumber) {
-    console.log("hai perso");
-    document.getElementById("esito").innerHTML = "HAI PERSO";
-    result.classList.add("lose");
-} else {
-    console.log("avete pareggiato");
-    document.getElementById("esito").innerHTML = "AVETE PAREGGIATO";
-    result.classList.add("draw");
-}
+    function () {
+
+    // far comparire il container
+
+
+    // stabilire le condizioni di vittoria, sconfitta e pareggio
+        if (humanNumber > computerNumber) {
+            console.log("hai vinto");
+            document.getElementById("esito").innerHTML = "HAI VINTO";
+            result.classList.add("win");
+        } else if (computerNumber > humanNumber) {
+             console.log("hai perso");
+            document.getElementById("esito").innerHTML = "HAI PERSO";
+            result.classList.add("lose");
+        } else {
+            console.log("avete pareggiato");
+            document.getElementById("esito").innerHTML = "AVETE PAREGGIATO";
+            result.classList.add("draw");
+        }
+
+        
+    }
+
+);
