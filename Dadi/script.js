72e8582fbd15ef4dc106c1b9bb69aa6ac144be65
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-22 17:12:19 +0100

    fix classList js (ora funziona)

--- Dadi/script.js
+++ Dadi/script.js
@@ -9,15 +9,20 @@ var computerNumber = (Math.floor(Math.random() * 6) + 1);
 console.log(computerNumber);
 document.getElementById("numero-computer").innerHTML = computerNumber;
 
+var result = document.getElementById("esito");
+
 
 // stabilire le condizioni di vittoria, sconfitta e pareggio
 if (humanNumber > computerNumber) {
     console.log("hai vinto");
-    document.getElementById("esito").innerHTML = "HAI VINTO"
+    document.getElementById("esito").innerHTML = "HAI VINTO";
+    result.classList.add("win");
 } else if (computerNumber > humanNumber) {
     console.log("hai perso");
-    document.getElementById("esito").innerHTML = "HAI PERSO"
+    document.getElementById("esito").innerHTML = "HAI PERSO";
+    result.classList.add("lose");
 } else {
     console.log("avete pareggiato");
-    document.getElementById("esito").innerHTML = "AVETE PAREGGIATO"
+    document.getElementById("esito").innerHTML = "AVETE PAREGGIATO";
+    result.classList.add("draw");
 }
