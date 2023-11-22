e8d608b28269cf39dbc18c5a224100c564d9baa8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-22 15:28:48 +0100

    modifiche ad html, css e js

--- Dadi/script.js
+++ Dadi/script.js
@@ -1,1 +1,6 @@
+// selezioniamo gli elementi del DOM
+const result = document.getElementById("esito");
+const humanNumber = document.getElementById("numero-umano");
+const computerNumber = document.getElementById("numero-computer");
+
 
